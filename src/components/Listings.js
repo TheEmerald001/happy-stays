8b354da89e4f0c38b401deb0554073cb8cb4be26
steve <austines811@gmail.com>
2022-09-09 18:23:11 +0300

    dummy html css web page implementation structure

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,18 +1,21 @@
-import React from "react";
+// import React, { usestate } from "react";
 
-function Listings(){
-    return (
-        <>
-            <div className="oneSpecies">
-                <img className="imgs" src = {image} alt='something'/>
-                <h5 className="speciesName">{name}</h5>
-                <p className="specieDescription">{desc}</p>
-                <div className="withBtn">
-                    <div id="donateBtnCarrier" className="withBtn"><button className="buttons" id="donateBtn"> Donate $10 </button></div>
-                </div>
-            </div>
-        </>
-    )
+// function Listings({listings}){
+//     const {name, image, city, price, rating} = listings;
+//     return (
+//         <>
+//             <div className="oneSpecies">
+//                 <img className="imgs" src = {image} alt='something'/>
+//                 <h5 className="speciesName">{name}</h5>
+//                 <h5>{city}</h5>
+//                 <h5>{price}</h5>
+//                 <h5>{rating}</h5>
+//                 <div className="withBtn">
+//                     <div id="donateBtnCarrier" className="withBtn"><button className="buttons" id="donateBtn"> Reserve </button></div>
+//                 </div>
+//             </div>
+//         </>
+//     )
 
-}
-export default Listings;
+// }
+// export default Listings;
